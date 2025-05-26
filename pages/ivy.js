9dbb31a703e2243651b7fafc18c15d69d484b1e3
winsonchristian <winsonanggoro@gmail.com>
2025-05-26 18:23:56 +0700

    add ivyOS page

---
+++ pages/ivy.js
@@ -0,0 +1,9 @@
+export default function IvyOSPage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🛠️ IvyOS</h1>
+      <p>Selamat datang, Ander!</p>
+      <p>Zona ini adalah ruang pelaksanaan visual: produksi, pengeditan, dan transfer konten dari strategi menjadi realita.</p>
+    </div>
+  );
+}
