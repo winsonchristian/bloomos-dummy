0db988e3a37cf290dc129c06e7d9bf588b2651eb
winsonchristian <winsonanggoro@gmail.com>
2025-05-26 18:19:18 +0700

    add lumenOS page

---
+++ pages/lumen.js
@@ -0,0 +1,9 @@
+export default function LumenOSPage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>💼 LumenOS</h1>
+      <p>Selamat datang, Andrew!</p>
+      <p>Di ruangan ini kamu bisa menavigasi arah bisnis, menyusun keputusan strategis, dan menjaga kestabilan Catha Lab sebagai CEO.</p>
+    </div>
+  );
+}
