51a0d16a23c7b5af1e2b4ce70897e1db28acf9d2
winsonchristian <winsonanggoro@gmail.com>
2025-05-26 18:17:15 +0700

    add vividOS page

---
+++ pages/vivid.js
@@ -0,0 +1,9 @@
+export default function VividOSPage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🎨 VividOS</h1>
+      <p>Selamat datang, Reza!</p>
+      <p>Ruang ini adalah tempat kamu menyusun, memberi arahan, dan mengevaluasi karya visual Catha Lab.</p>
+    </div>
+  );
+}
