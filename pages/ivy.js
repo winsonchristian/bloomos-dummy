4d7db7953b950fd5f9cdb36839e797baf1470368
winsonchristian <winsonanggoro@gmail.com>
2025-05-26 20:05:24 +0700

    Update ivy.js

--- pages/ivy.js
+++ pages/ivy.js
@@ -3,7 +3,7 @@ export default function IvyOSPage() {
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>🛠️ IvyOS</h1>
       <p>Selamat datang, Ander!</p>
-      <p>Zona ini adalah ruang pelaksanaan visual: produksi, pengeditan, dan transfer konten dari strategi menjadi realita.</p>
+      <p>Saat ini aku tidak bisa menampilkan lebih banyak karena masih ada deadline yang harus kamu selesaikan. (Aku juga takut dimarahi Dhani)</p>
     </div>
   );
 }
