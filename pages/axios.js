940881de1b4f7440265ff56d2bbff2eaea816435
winsonchristian <winsonanggoro@gmail.com>
2025-05-26 18:18:18 +0700

    add axiosOS page

---
+++ pages/axios.js
@@ -0,0 +1,9 @@
+export default function AxiosOSPage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>📋 AxiosOS</h1>
+      <p>Selamat datang, Dhani!</p>
+      <p>Zona ini adalah pusat log kerja, ritme operasional, dan laporan harian Catha Lab.</p>
+    </div>
+  );
+}
