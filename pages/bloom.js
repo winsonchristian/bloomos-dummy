21704dd8ea2fd8e36f1d053327fb513a6401dcc6
winsonchristian <winsonanggoro@gmail.com>
2025-05-26 18:16:01 +0700

    add bloomOS page

---
+++ pages/bloom.js
@@ -0,0 +1,9 @@
+export default function BloomOSPage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🌿 BloomOS</h1>
+      <p>Selamat datang, Irvinne!</p>
+      <p>Di sinilah konten Catha Lab bertumbuh dan disusun 🌱✨</p>
+    </div>
+  );
+}
